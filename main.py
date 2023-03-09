201021c918bdf3a1879dfb35ff3e5028ed288c9f
kidcarlos <126624346+KidCarlos@users.noreply.github.com>
2023-03-09 22:18:28 +0000

    Recomplete E4

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    celcius = int(input('Enter the Celcius Degree: '))
-    fahrenheit = (9.0 / 5.0) * celcius + 32
-    print(f'The Fahrenheit is {fahrenheit:.2f}')
-    pass
+  celcius = int(input('Enter the Celcius Degree: '))
+  fahrenheit = (9.0 / 5.0) * celcius + 32
+  print(f'The Fahrenheit is {fahrenheit:.2f}')
+  pass
 
 
 if __name__ == '__main__':
